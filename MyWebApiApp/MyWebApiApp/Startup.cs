d6cae89291d3d6d4fccf9d911cc83d7a06349c49
duongmake <duongmake3@gmail.com>
2024-01-08 14:15:01 +0700

    [WebAPI-NET5] #6 Repository Pattern in ASP.NET Core Web API

--- MyWebApiApp/MyWebApiApp/Startup.cs
+++ MyWebApiApp/MyWebApiApp/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using MyWebApiApp.Data;
+using MyWebApiApp.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -36,6 +37,8 @@ namespace MyWebApiApp
             {
                 option.UseSqlServer(Configuration.GetConnectionString("MyDB"));
             });
+
+            services.AddScoped<ILoaiRepository, LoaiRepositoryInMemory>();
 
 
             services.AddSwaggerGen(c =>
